bf3eef15af4f34bd6554d9a5a6942528bb2245ed
李琪琦 <445072942@qq.com>
2018-05-20 13:02:00 +0800

    修改fliter null的逻辑

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ const alfy = require('alfy')
 const latestUri = '122.152.219.33:5000/latest'
 
 alfy.fetch(latestUri).then(data => {
-	const items = data.map(x => {
-		if (x) {
-			return {
-				title: x.name,
-				subtitle: x.introduction,
-				arg: x.uri,
+	const items = []
+	for (let index = 0; index < data.length; index += 1) {
+		if (data[index]) {
+			items.push({
+				title: data[index].name,
+				subtitle: data[index].introduction,
+				arg: data[index].uri,
 				icon: {
 					path: ""
 				}
-			}
+			})
 		}
-	})
+	}
 	alfy.output(items)
 })
